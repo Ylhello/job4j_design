432dca23dfe6c213ec806a8fa10233325d4c7729
Yulya <ylhappy999@gmail.com>
2025-05-25 01:53:03 +1000

    Импорт задачи "Без переопределения equals и hashCode" на Github [1005]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,16 +1,30 @@
 package ru.job4j.map;
 
-import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
     private String name;
     private int children;
-    private Calendar birthday;
 
-    public User(String name, int children, Calendar birthday) {
+    public User(String name, int children) {
         this.name = name;
         this.children = children;
-        this.birthday = birthday;
+    }
 
+    @Override
+    public String toString() {
+        return "User{name='" + name + "', children=" + children + "}";
+    }
+
+    public static void main(String[] args) {
+        Map<User, Object> map = new HashMap<>();
+        User user1 = new User("Marina", 2);
+        User user2 = new User("Olga", 1);
+
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+
+        System.out.println(map);
     }
 }
